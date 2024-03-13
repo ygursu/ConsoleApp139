516bbe889db0ccf6cb346d394b4e0ea6e269cb2b
ygursu <yasemingrs@gmail.com>
2024-03-13 23:20:27 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp139/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Birinci sayıyı giriniz:");
+A = Convert.ToDouble(Console.ReadLine());
+Console.Write("İkinci sayıyı giriniz:");
+B = Convert.ToDouble(Console.ReadLine());
+
+YEDEK = A;
+A = B;
+B = YEDEK;
+
+Console.WriteLine("Değişken içerikleri değiştirilmiştir.");
+Console.ReadKey();
